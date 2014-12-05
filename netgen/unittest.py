ec5401d7eaaac2875099316d7a317fc55b40e601
Nicolas Limage <github@xephon.org>
2014-12-05 12:20:18 +0100

    updated unittests

--- netgen/unittest.py
+++ netgen/unittest.py
@@ -4,7 +4,7 @@ import netgen
 class Host(unittest.TestCase):
 
     def setUp(self):
-        self.host = netgen.Host('test', '192.168.2.34', None)
+        self.host = netgen.Host('test', '192.168.2.34')
 
     def test_host_name(self):
         self.assertEqual(self.host.name, 'test')
