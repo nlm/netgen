6e870ea414785b044439076cc3a01f673f5ed536
Nicolas Limage <github@xephon.org>
2016-04-18 12:29:16 +0200

    bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
     name = "netgen",
-    version = "0.4b10",
+    version = "0.4b11",
     packages = ['netgen'],
     author = "Nicolas Limage",
     author_email = 'github@xephon.org',
