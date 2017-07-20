4e906c081ef0ab9f7d8b688be0de368069d49219
Nicolas Limage <github@xephon.org>
2017-07-20 15:21:55 +0200

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
     name = "netgen",
-    version = "0.4b12",
+    version = "0.5.0-beta1",
     packages = find_packages(),
     author = "Nicolas Limage",
     author_email = 'github@xephon.org',
